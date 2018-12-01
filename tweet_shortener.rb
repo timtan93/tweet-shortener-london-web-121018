8d1045b5ae6f97e3b964ffa376dde71c67cdee07
Timothy Tan <timtan93@gmail.com>
2018-12-01 11:07:51 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+ def dictionary
+   subs ={
+       "hello" => 'hi',
+     "to" => '2',
+     "two" => '2',
+     "too" => '2',
+     "for" => '4', 
+     "four" => '4',
+     'be' => 'b',
+     'you' => 'u',
+     "at" => "@",
+     "and"  =>"&"
+   }
+   
+   def word_substituter(tweets)
+     
+     tweets.split.collect do |word|
+       if dictionary.keys.include?(word.downcase)
+         word = [dictionary.downcase]
